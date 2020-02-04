4a7973effa0a418c4bd44a8b097eba48ec522733
KimBamYa <recon7347@gmail.com>
2020-02-05 00:09:20 +0900

    이동경로

--- Assets/00.Scripts/EnemyControl.cs
+++ Assets/00.Scripts/EnemyControl.cs
@@ -18,4 +18,18 @@ public class EnemyControl : MonoBehaviour
     {
         
     }
+
+
+
+      private void OnDrawGizmos()
+    {
+        Vector2 limitPoint3 = new Vector2(limitPoint2.x,limitPoint1.y); 
+        Vector2 limitPoint4 = new Vector2(limitPoint1.x,limitPoint2.y);
+        Gizmos.color =Color.red;
+        Gizmos.DrawLine(limitPoint1,limitPoint3);
+        Gizmos.DrawLine(limitPoint3,limitPoint2);
+        Gizmos.DrawLine(limitPoint1,limitPoint4);
+        Gizmos.DrawLine(limitPoint4,limitPoint2);
+    }
+
 }
