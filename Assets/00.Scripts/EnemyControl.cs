28f4357e5f6c40d01951e05259bbd840700a5985
KimBamYa <recon7347@gmail.com>
2020-02-05 00:06:22 +0900

    no message

--- Assets/00.Scripts/EnemyControl.cs
+++ Assets/00.Scripts/EnemyControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+    public Vector2 limitPoint1;
+    public Vector2 limitPoint2;
+
     // Start is called before the first frame update
     void Start()
     {
